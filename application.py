a5f2dac6112f0f9cb6691fcee9f2cfc6daf28361
phayo <mrphayo@MrPhayo-PC.localdomain>
2019-10-01 22:49:53 +0100

    Set up all imports, routes, environmental variables and error handling needed for project

---
+++ application.py
@@ -0,0 +1,91 @@
+import os
+
+from cs50 import SQL
+from flask import Flask, flash, jsonify, redirect, render_template, request, session
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from helpers import apology, login_required, lookup, usd
+
+# Configure application
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 50
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+# Custom filter
+app.jinja_env.filters["usd"] = usd
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///finance.db")
+
+# Make sure API key is set
+if not os.environ.get("API_KEY"):
+    raise RuntimeError("API_KEY not set")
+
+
+@app.route("/")
+@login_required
+
+
+@app.route("/buy", methods=["GET", "POST"])
+@login_required
+
+
+@app.route("/check", methods=["GET"])
+
+
+@app.route("/history")
+@login_required
+
+
+@app.route("/register", methods=["GET", "POST"])
+
+
+@app.route("/login", methods=["GET", "POST"])
+
+@app.route("/logout")
+
+
+
+@app.route("/quote", methods=["GET", "POST"])
+@login_required
+
+
+
+# redirect if user attempts to call quoted with query
+@app.route("/quoted", methods=["GET"])
+@login_required
+
+
+@app.route("/sell", methods=["GET", "POST"])
+@login_required
+
+
+def errorhandler(e):
+    """Handle error"""
+    if not isinstance(e, HTTPException):
+        e = InternalServerError()
+    return apology(e.name, e.code)
+
+
+# Listen for errors
+for code in default_exceptions:
+    app.errorhandler(code)(errorhandler)
